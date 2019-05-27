93a52ff34c16c0d4945e7f223f40456b39ea51f4
Diogo Reis <up201505472@fe.up.pt>
2019-05-27 01:53:03 +0100

    DB now works

--- backend/models/data.js
+++ backend/models/data.js
@@ -9,4 +9,12 @@ const DataSchema = new Schema(
   { timestamps: true }
 );
 
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+});
+
+
 module.exports = mongoose.model("Data", DataSchema);
